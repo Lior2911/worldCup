b9020a7f6d00b283c695b9c31ba8c837f36ed6e1
lior2911 <aytegev1@gmail.com>
2022-07-31 14:54:20 +0300

    nituv2

--- pages/main/article.js
+++ pages/main/article.js
@@ -1,31 +1,26 @@
-// const NEWSAPI = "https://community-official-world-cup.p.rapidapi.com/wc/matches";
-// async function getNewws(){
-//   try{
-//     return await fetch(NEWSAPI,{
-//       method: 'GET',
-//       headers: {
-//         'X-RapidAPI-Key': 'a3cd827027msh0132ea0897450e5p1a2c87jsncbbc1fd35305',
-//         'X-RapidAPI-Host': 'community-official-world-cup.p.rapidapi.com'
-//       }
 
-//     }).then(response =>
-//       response.json()).then(res =>
-//         console.log(res.id))
-//   }
-//   catch(err){
-//     console.log(err);
-//   }
-// }
-// getNewws()
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'a3cd827027msh0132ea0897450e5p1a2c87jsncbbc1fd35305',
-		'X-RapidAPI-Host': 'live-score-api.p.rapidapi.com'
+
+	async function getCovid(){
+		try{
+			return await fetch('https://covid-193.p.rapidapi.com/statistics?country=Qatar',{
+				method: 'GET',
+				headers: {
+						'X-RapidAPI-Key': 'a3cd827027msh0132ea0897450e5p1a2c87jsncbbc1fd35305',
+						'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
+							}
+			})
+			.then(response => response.json())
+			.then(res => res)
+			.then(myArray => {for (const key in myArray) {
+				console.log(key);
+			
+			}}
+				
+				
+		)
+		}
+		catch(err){
+			console.log(err)
+		}
 	}
-};
-
-fetch('https://live-score-api.p.rapidapi.com/federations/list.json?secret=undefined&key=undefined', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	getCovid()
